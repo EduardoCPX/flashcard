55e0aa2b7da76006e58566334c1e0199782648ae
EduardoCPX <teleken.peres@escola.pr.gov.br>
2025-05-28 08:57:49 -0300

    Add files via upload

---
+++ flashcard_2°B/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Esportes',
+    'Onde foi inventado o futebol?',
+    'O futebol foi inventado na inglaterra'
+)
+
+criaCartao(
+    'Jogos eletrônicos',
+    'Qual é o jogo mais antigo do mundo?',
+    'o jogo mais antigo do mundo é The oregon trail'
+)
+
+criaCartao(
+    'Jogos eletrônicos',
+    'Quem são os "traidores de" GTA san andreas?',
+    'Ryder e Big smoke'
+)
